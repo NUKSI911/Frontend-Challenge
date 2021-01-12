ed19f432929d9910cc446d821b319993fa851863
Nurudeen Yekeen <nuksi911@gmail.com>
2021-01-12 12:37:39 +0100

    testing production

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const port = process.env.PORT || 3000;
 const app = express();
-const publicPath = path.join(__dirname ,"../../../","build");
+const publicPath = path.join(__dirname ,"..","build");
 
 app.use(express.static(publicPath));
 
